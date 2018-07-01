6e93cf5267c79620924e7b903961f257759ec34e
walkor <walkor@users.noreply.github.com>
2018-07-01 20:55:11 +0800

    Update Mqtt.php

--- Protocols/Mqtt.php
+++ Protocols/Mqtt.php
@@ -11,7 +11,7 @@
  * @link      http://www.workerman.net/
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
  */
-namespace Workerman\MQTT\Protocols;
+namespace Workerman\Mqtt\Protocols;
 
 /**
  * Mqtt Protocol.
