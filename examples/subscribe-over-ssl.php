1dd4d4c0a9f0e7b5335b5f6497ef8ce9872d623e
walkor <walkor@users.noreply.github.com>
2018-07-06 15:19:56 +0800

    Create subscribe-over-ssl.php

---
+++ examples/subscribe-over-ssl.php
@@ -0,0 +1,23 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+use Workerman\Worker;
+
+$worker = new Worker();
+$worker->onWorkerStart = function(){
+    $mqtt = new Workerman\Mqtt\Client('mqtts://test.mosquitto.org:8883', array(
+        'ssl' => array(
+            'local_cert'  => __DIR__ . '/ssl/mosquitto.org.crt',
+            'local_pk'    => __DIR__ . '/ssl/mosquitto.org.key',
+            'verify_peer' => false,
+        ),
+        'debug' => true
+    ));
+    $mqtt->onConnect = function($mqtt) {
+        $mqtt->subscribe('test');
+    };
+    $mqtt->onMessage = function($topic, $content){
+        var_dump($topic, $content);
+    };
+    $mqtt->connect();
+};
+Worker::runAll();
